807c07598946ba508894f50b465c98566deed23e
Jonas Capelier <capelier.jonas@gmail.com>
2025-07-17 17:14:07 +0200

    Force JS assets to be served as application/javascript

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,3 +95,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
+  # Ensure JS assets are served with the correct MIME type for ES modules
+  config.assets.configure do |env|
+    env.register_mime_type 'application/javascript', extensions: ['.js'], charset: :unicode
+  end
